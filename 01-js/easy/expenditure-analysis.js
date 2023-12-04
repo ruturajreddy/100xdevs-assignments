3418846fd878714932d11a15a1fd75b5f5fd134d
Ruturaj Reddy <69036453+ruturajreddy@users.noreply.github.com>
2023-12-04 20:29:22 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -5,8 +5,34 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
-}
+// function calculateTotalSpentByCategory(transactions) {
+//   return [{category : transactions['category'], amountspent : transactions['price']}];
+// }
 
-module.exports = calculateTotalSpentByCategory;
+// module.exports = calculateTotalSpentByCategory;
+
+const transaction = [{
+  itemName : 'milk', 
+  category : 'grocery', 
+  price : 1000, 
+  timestamp : 'today'
+}, {
+  itemName : 'bread', 
+  category : 'grocery', 
+  price : 1000, 
+  timestamp : 'today'
+}, {
+  itemName : 'clothes', 
+  category : 'wearables', 
+  price : 1000, 
+  timestamp : 'today'
+}
+]
+
+// function calTotalAmt(transaction) {
+//   return transaction['itemName'];
+// }
+
+// console.log(calTotalAmt(transaction));
+
+console.log(transaction[0]['category']);
