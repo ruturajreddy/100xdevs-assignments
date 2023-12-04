0ab3046baf8f431d3f5f9fc7d47edf2a4515a0d0
Ruturaj Reddy <69036453+ruturajreddy@users.noreply.github.com>
2023-12-04 20:27:26 +0530

    Update times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -7,7 +7,15 @@ Try running it for
 Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
-
+let start = Date.now()
 function calculateTime(n) {
-    return 0.01;
+    let sum = 0;
+    for ( x=0; x<n; x++ ) {
+        sum += x;
+    }
+    return sum;
 }
+calculateTime(1000000000);
+let end = Date.now()
+console.log(start, end);
+console.log(end-start);
